d99fe4d292361534f52136861960b60711ae494f
Xetactf <107368569+Xetactf@users.noreply.github.com>
2023-05-05 08:33:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ package slot;
 
 public class Main {
 	public static void main(String[] args) {
+		//Creation d'une machine
 		Machine machine = new Machine();
+		//creation d'un utilisateur 
 		User user = new User(machine);
+		//Afficher le solde au démmarage
 		user.printSolde();
+		//Lancer la machine
 		user.lancerMachine();
 	}
 
 }
-
