4a3a47251f590071d5800a59e82c85bcee3eba64
Xetactf <107368569+Xetactf@users.noreply.github.com>
2023-05-05 08:33:37 +0200

    Update Rouleau.java

--- Rouleau.java
+++ Rouleau.java
@@ -3,24 +3,28 @@ package slot;
 
 import java.util.Random;
 
-public class Rouleau {
-    private Symbole[] listeSymboles;
-    private int position;
-    private int taille;
-    private boolean freesymb;
-    private boolean supersymb;
-
-    public Rouleau(int taille, boolean freesymb, boolean supersymb) {
-    this.taille = taille;
-    this.listeSymboles = new Symbole[taille]; // Initialisation du tableau
-    for (int i = 0; i < taille; i++) {
-        listeSymboles[i] = new Symbole(freesymb, supersymb);
-    }
-    this.position = 0;
-    this.freesymb = freesymb;
-    this.supersymb = supersymb;
-}
-
+	public class Rouleau {
+	    private Symbole[] listeSymboles;
+	    private int position;
+	    private int taille;
+	    private boolean freesymb;
+	    private boolean supersymb;
+	    
+	    //Methode d'initialisation d'un rouleau
+	    public Rouleau(int taille, boolean freesymb, boolean supersymb) {
+	    this.taille = taille;
+	    this.listeSymboles = new Symbole[taille];
+	    
+	    for (int i = 0; i < taille; i++) {
+	        listeSymboles[i] = new Symbole(freesymb, supersymb);
+	    }
+	    
+	    this.position = 0;
+	    this.freesymb = freesymb;
+	    this.supersymb = supersymb;
+	}
+    
+	//Methode pour retourner le symbole à une position X par rapport à la position actuelle
     public Symbole getSymboleAtPosition(int position) {
         if(this.position + position >= this.taille) {
             return this.listeSymboles[(this.position + position) - this.taille];
@@ -30,7 +34,8 @@ public class Rouleau {
             return this.listeSymboles[this.position + position];
         }
     }
-
+    
+    //Tourner le rouleau
     public void roll(int nombreAleatoire) {
         if(nombreAleatoire + this.position > this.taille) {
             this.position = (nombreAleatoire + this.position) - this.taille;
@@ -38,16 +43,5 @@ public class Rouleau {
             this.position += nombreAleatoire;
         }
     }
-
-    public void updatesymbatpos(int position){
-        if(this.position + position > this.taille) {
-            this.listeSymboles[(this.position + position) - this.taille] = new Symbole(this.freesymb, this.supersymb);
-        }else if(this.position + position < 0) {
-            this.listeSymboles[this.taille + (this.position + position)] = new Symbole(this.freesymb, this.supersymb);
-        } else {
-            this.listeSymboles[this.position + position] = new Symbole(this.freesymb, this.supersymb);
-        }
-    }
     
 }
-
